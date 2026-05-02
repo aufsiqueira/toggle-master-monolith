54db303b1f68f4d2ba13a3dc3be9d9b33eb782d1
A.Siqueira <50467220+aufsiqueira@users.noreply.github.com>
2026-05-02 01:29:33 -0300

    Update test_db.py

--- test_db.py
+++ test_db.py
@@ -1,20 +1,59 @@
 import os
+import json
+import click
+import boto3
+from botocore.exceptions import ClientError
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-# Pega as env vars que você exportou
-DB_HOST = os.getenv('DB_HOST')
-DB_NAME = os.getenv('DB_NAME')
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
+# Nome/ARN do secret e região
+SECRET_NAME = "techchallenge/3DCLT/app/togglemaster"
+REGION_NAME = "us-east-1"
+
+def load_db_credentials_from_secret():
+    if not SECRET_NAME:
+        raise RuntimeError("DB_SECRET_NAME não definido nas variáveis de ambiente")
+
+    session = boto3.session.Session()
+    client = session.client(service_name="secretsmanager", region_name=REGION_NAME)
+
+    try:
+        response = client.get_secret_value(SecretId=SECRET_NAME)
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'ResourceNotFoundException':
+            print("The requested secret " + SECRET_NAME + " was not found")
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            print("The request was invalid due to:", e)
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            print("The request had invalid params:", e)
+        elif e.response['Error']['Code'] == 'DecryptionFailure':
+            print("The requested secret can't be decrypted using the provided KMS key:", e)
+        elif e.response['Error']['Code'] == 'InternalServiceError':
+            print("An error occurred on service side:", e)
+
+    # SecretString (texto)
+    secret_string = response.get("SecretString")
+    if not secret_string:
+        raise RuntimeError(f"Secret {SECRET_NAME} não contém campo SecretString")
+
+    secret_dict = json.loads(secret_string)
+
+    return {
+        "host": secret_dict["host"],
+        "dbname": secret_dict.get("dbname", "postgres"),
+        "user": secret_dict["username"],
+        "password": secret_dict["password"],
+        "port": secret_dict.get("port", 5432),
+    }
 
 try:
+    creds = load_db_credentials_from_secret()
     conn = psycopg2.connect(
-        host=DB_HOST,
-        database=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        port=5432  # padrão PostgreSQL
+        host= creds["host"],
+        database= creds["dbname"],
+        user= creds["user"],
+        password= creds["password"],
+        port= creds["port"]
     )
     conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     cur = conn.cursor()
